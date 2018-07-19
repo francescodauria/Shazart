07c15c70d6293d685723c47fafdadd9b03615631
Unknown <f.dauria37@studenti.unisa.it>
2018-07-19 17:35:53 +0200

    Aggiunti cambiamenti photo-information, cambiato colore status-bar e aggiunto pulsante salva in profilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,7 +53,7 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
+      this.statusBar.backgroundColorByHexString('#183d4f');
       this.splashScreen.hide();
     });
   }
